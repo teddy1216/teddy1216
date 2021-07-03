5fc134930f475a6d26011c0ffcff859f7435298a
teddy1216 <86868625+teddy1216@users.noreply.github.com>
2021-07-03 06:26:15 -0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb://tar:jqwer@cluster0-shard-00-00.b2mfm.mongodb.net:27017,cluster0-shard-00-01.b2mfm.mongodb.net:27017,cluster0-shard-00-02.b2mfm.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
+};
